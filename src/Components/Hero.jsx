e9d0c4bf88ce00c762844cb76c9a7caf920c037b
Prabhat Dwivedi <160233198+PrabhatCodess@users.noreply.github.com>
2024-11-29 00:54:58 +0530

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className=' w-full h-[90vh] bg-[url("/Cara/public/hero4.png")] bg-no-repeat bg-cover flex flex-col items-start justify-center px-28 mb-10 gap-1 tablets:max-desktop:px-10 tablets:max-desktop:bg-[right_30%_top_10%] tablets:max-desktop:h-[70vh] mobile:max-tablets:px-2 mobile:max-tablets:py-1 mobile:max-tablets:bg-[right_35%_top_10%]'>
+    <div className=' w-full h-[90vh] bg-[url("/hero4.png")] bg-no-repeat bg-cover flex flex-col items-start justify-center px-28 mb-10 gap-1 tablets:max-desktop:px-10 tablets:max-desktop:bg-[right_30%_top_10%] tablets:max-desktop:h-[70vh] mobile:max-tablets:px-2 mobile:max-tablets:py-1 mobile:max-tablets:bg-[right_35%_top_10%]'>
         <h4 className='text-4xl font-semibold mobile:max-tablets:text-3xl'>Trade-in-offer</h4>
         <h2 className='text-6xl font-bold mobile:max-tablets:text-3xl'>Super Value Deals</h2>
         <h1 className='text-6xl font-bold text-[#088178] mobile:max-tablets:text-3xl'>On All Products</h1>
