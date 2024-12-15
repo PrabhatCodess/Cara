b890c685de80254c1d9b7f2b68ebb4098f591c32
[PrabhatCodess] <[ak1973divedi@gmail.com]>
2024-12-16 00:53:21 +0530

    setting up vercel

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/Cara/', // Replace <REPO_NAME> with your GitHub repository name
   build: {
+    outDir: 'dist',
     rollupOptions: {
       external: [
         '/Cara/public/features/f2.png'
