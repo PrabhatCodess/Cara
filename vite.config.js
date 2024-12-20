79692de7d4da8fbae1b25aa24e17314fb001310d
[PrabhatCodess] <[ak1973divedi@gmail.com]>
2024-12-20 22:38:01 +0530

    vercel deply fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Cara/', // Ensures assets are resolved from /Cara/
+  base: '/', // Ensures assets are resolved from /Cara/
   plugins: [react()]
 });
 
