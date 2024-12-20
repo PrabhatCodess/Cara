f4406b9e70a892339a5e69335934cf15d54afe39
[PrabhatCodess] <[ak1973divedi@gmail.com]>
2024-12-20 18:41:06 +0530

    added home page videos and other changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Replace <REPO_NAME> with your GitHub repository name
+  base: '/Cara/', // Replace <REPO_NAME> with your GitHub repository name
   
   build: {
     rollupOptions: {
