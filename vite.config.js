9b78fbfbbdfd377c52f0338ac2ba5a87cb51cdf2
[PrabhatCodess] <[ak1973divedi@gmail.com]>
2024-12-23 06:22:33 +0530

    git fixes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Ensures assets are resolved from /Cara/
-  plugins: [react()]
+  plugins: [react()],
+  base: process.env.VERCEL ? "/" : "/Cara/",
 });
-
