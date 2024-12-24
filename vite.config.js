a360320f7598e0964febfd31d6ddd426372c4c23
[PrabhatCodess] <[ak1973divedi@gmail.com]>
2024-12-24 05:46:55 +0530

    fixing deploy issues

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
-
-// Load environment variables from .env file
-dotenv.config();
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Cara/' : '/',
+  base: "/"
 });
